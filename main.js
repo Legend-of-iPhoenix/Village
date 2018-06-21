ddb7a5dd1173377e9d96387154d4de28d2e2233c
_iPhoenix_ <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2018-06-21 13:51:02 -0400

    Add conditionals and quality-of-life things.

--- main.js
+++ main.js
@@ -33,6 +33,7 @@ Tell Edsger to write the text "example" on her scroll.
 Note: If you think I'm being an jerk because of this, 
 Note: check the comment here: https://github.com/Legend-of-iPhoenix/Village/blob/a8d53ad79a76b9bb129c75165c4a5ad67179e25d/main.js#L89
 */
+var DEBUG = true, logToConsole;
 
 window.onload = function () {
   var codeInput = document.getElementById("input")
@@ -50,32 +51,38 @@ window.onload = function () {
   codeInput.onscroll = function (event) {
     lineNumbers.scrollTop = codeInput.scrollTop;
   }
+
   var row_number = 0;
+
   lineNumbers.innerHTML = "";
   codeInput.value.split("\n").forEach(function (row) {
     row_number++;
     lineNumbers.innerHTML += row_number + "\n";
   });
   lineNumbers.cols = Math.floor(1 + Math.log10(row_number));
+  document.getElementById("showConsole").onchange = function (event) {
+    if (event.target.checked) {
+      document.getElementById("consoleHeader").style.display = document.getElementById("console").style.display = "block";
+    } else {
+      document.getElementById("consoleHeader").style.display = document.getElementById("console").style.display = "none";
+    }
+    DEBUG = event.target.checked;
+
+  }
 }
-
-
-
-
-var DEBUG = true;
 
 var occupations = {
   "lumberjack": "wood",
   "quarryman": "stone",
   "architect": "blueprint",
-  "builder": "None"
+  "builder": false // the builder builds buildings, but doesn't have its own item. This is easy to check for.
 }
 
 var occupationActions = {
-  "lumberjack": "gather (\\d+) wood",
+  "lumberjack": "(?:gather|collect|harvest) (\\d+) wood",
   "quarryman": "(?:mine|quarry) (\\d+) stone",
   "architect": "(?:create|draft|make) a blueprint for a structure (?:(?:that requires)|(?:requiring)) (\\d+|no) wood and (\\d+|no) stone",
-  "builder": "build a structure using (\\w+)'s blueprint, (\\w+)'s wood, and (\\+) stone."
+  "builder": "build a structure using (\\w+)'s blueprint, (\\w+)'s wood, and (\\+) stone"
 }
 
 var generalActions = [
@@ -120,6 +127,28 @@ var generalActions = [
         return false;
       }
     }
+  ],
+  ["write the (?:amount|number) of (\\w+) (\\w+) has on (\\w+) scroll",
+    function (matches, villager, line) {
+      if (matches[2] == villager.genderPronoun2) {
+        if (matches[3] == villager.genderPronoun) {
+          if ((matches[1] == villager.specialItemType || (matches[1] == villager.specialItemType + 's')) && villager.specialItemType && villager.specialItem) {
+            villager.scroll.text += villager.specialItem.quantity + '';
+            return true;
+          } else {
+            villager.scroll.text += "0"
+            return true;
+          }
+          logToConsole(villager.name + " successfully wrote the amount of " + matches[1] + " that " + villager.genderPronoun2 + " has on "+ villager.genderPronoun + " scroll.");
+        } else {
+          logToConsole("Error on line " + (line + 1) + ": " + villager.name + " is " + villager.gender + ' and prefers that you use "' + villager.genderPronoun + '" over "' + matches[3] + '".');
+          return false;
+        }
+      } else {
+        logToConsole("Error on line " + (line + 1) + ": " + villager.name + " is " + villager.gender + ' and prefers that you use "' + villager.genderPronoun2 + '" over "' + matches[2] + '".');
+        return false;
+      }
+    }
   ]
 ];
 
@@ -161,7 +190,13 @@ function Scroll(text) {
 }
 
 function postToMessageBoard(scroll) {
-  document.getElementById("messageBoard").value += scroll.text;
+  document.getElementById("messageBoard").value += scroll.text + '\n';
+}
+
+function Routine(name, start, end) {
+  this.name = name;
+  this.start = start;
+  this.end = end;
 }
 
 // I'm choosing names of people who had an impact on computer science, programming, or mathematics. Generally, I'm using names that sound relatively English. Sorry Ramanujan.
@@ -171,7 +206,7 @@ var malevillagers = [
   "Charles", // Babbage
   "Dennis", // Ritchie
   "Edsger", // Dijkstra, might be a bit of a stretch for my final rule.
-  "Felix",
+  "Felix", // placeholder
   "George", // Boole
   "John", // von Neumann
   "Ken", // Thompson
@@ -179,19 +214,40 @@ var malevillagers = [
   /* TODO: add more names */
 ]
 
-/* TODO: add female villagers */
+var femalevillagers = [
+  "Ada" // Lovelace. This was a natural choice.
+  /* TODO: add more names */
+]
 
 var villagers = {};
+var routines = {};
 
-function parse() {
+
+// I should probably make an AST instead of using a bunch of if statements and regexes, but this isn't going to be that complex of a language.
+// Well, it'll be very complex, but not in the way that an AST helps with.
+function run() {
+  if (!DEBUG) {
+    logToConsole = x => {}
+  } else {
+    logToConsole = function (text) {
+      document.getElementById("console").value += '> ' + text + "\n";
+    }
+  }
   var text = document.getElementById('input').value;
   villagers = {};
   document.getElementById("messageBoard").value = "";
   document.getElementById("console").value = "";
   var line = 0;
   var commands = text.split('\n');
+  var indentLevel = 0;
   while (line < commands.length) {
     var command = commands[line];
+    var numSpaces = command.match(/^ */)[0].length;
+    if (numSpaces && !command.substring(numSpaces).startsWith('-+*'.charAt((numSpaces-1)%3))) {
+      logToConsole("Indentation Error on line " + (line+1) + ": Expected '" + '-+*'.charAt((numSpaces-1)%3) + "', recieved '" + command.charAt(numSpaces + 1));
+    }
+    indentLevel = numSpaces;
+    command = command.substring(indentLevel + (!!indentLevel)).trim();
     var didCommand = false;
     if (!command.toLowerCase().startsWith("note: ") && command.length) {
       if (command.startsWith("Call")) {
@@ -203,7 +259,13 @@ function parse() {
             didCommand = true;
             villagers[match] = new Villager(match, "male", occupationNames[malevillagers.indexOf(match) % occupationNames.length]);
           } else {
-            logToConsole("That villager is unavailable or you already called for that villager.");
+            if (femalevillagers.indexOf(match) != -1 && !villagers[match]) {
+              logToConsole("Successfully called for the villager named " + match + ".");
+              didCommand = true;
+              villagers[match] = new Villager(match, "female", occupationNames[femalevillagers.indexOf(match) % occupationNames.length]);
+            } else {
+              logToConsole("That villager is unavailable or you already called for that villager.");
+            }
           }
         } else {
           logToConsole("Syntax Error on line " + (line + 1) + ": Should be \"Call for the villager named <name>.\", where <name> is an villager name. There is a list of valid villager names in the documentation.");
@@ -253,11 +315,72 @@ function parse() {
                 var successCallback = data[1];
                 match = action.match(new RegExp(actionRegEx));
                 if (match) {
+                  console.log(match);
                   if (successCallback(match, villager, line)) {
                     didCommand = true;
                   }
                 }
               });
+            }
+          }
+        } else {
+          if (command.startsWith("Ask")) {
+            var matches = command.match(/Ask (\w+) if (\w+) has (any|\d+) (\w+)s?\./);
+            if (matches) {
+              var villager = villagers[matches[1]];
+              var pronoun = matches[2];
+              var amount = matches[3] == "any" ? 1 : parseInt(matches[3]);
+              var material = matches[4];
+              if (villager && villager.cooldown === 0) {
+                if (pronoun == villager.genderPronoun2) {
+                  indentLevel++;
+                  var lookingFor = "doesn't";
+                  if (villager.specialItemType == material && villager.specialItem && villager.specialItem.quantity >= amount) {
+                    lookingFor = "does";
+                  }
+                  lookingFor = "If " + villager.genderPronoun2 + " " + lookingFor + ":";
+                  var indentLevel2 = indentLevel;
+                  var line2 = line;
+                  var threwError = false;
+                  while (line2 < commands.length && !(commands[line2].substring(indentLevel2 + (!!indentLevel2)).trim().startsWith(lookingFor) && indentLevel2 == indentLevel) && indentLevel2 >= indentLevel) {
+                    line2++;
+                    var numSpaces = commands[line2].match(/^ */)[0].length;
+                    if (numSpaces && !commands[line2].substring(numSpaces).startsWith('-+*'.charAt((numSpaces-1)%3))) {
+                      logToConsole("Indentation Error on line " + (line+1) + ": Expected '" + '-+*'.charAt((numSpaces-1)%3) + "', recieved '" + commands[line2].charAt(numSpaces + 1));
+                      threwError = true;
+                      break
+                    }
+                    indentLevel2 = numSpaces;
+                  }
+                  if (line2 < commands.length && !threwError) {
+                    line = line2;
+                    didCommand = true;
+                  }
+                } else {
+                  logToConsole("Error on line " + (line + 1) + ": " + villager.name + " is " + villager.gender + ' and prefers that you use "' + villager.genderPronoun2 + '" over "' + matches[2] + '".');
+                }
+              } else {
+                logToConsole("You have not called for that villager yet or that villager is unavailable.");
+              }
+            }
+          } else {
+            if (command.startsWith("If")) {
+              var indentLevel2 = indentLevel;
+              var line2 = line;
+              var threwError = false;
+              while (line2 < commands.length && indentLevel2 >= indentLevel) {
+                line2++;
+                var numSpaces = commands[line2].match(/^ */)[0].length;
+                if (numSpaces && !commands[line2].substring(numSpaces).startsWith('-+*'.charAt((numSpaces-1)%3))) {
+                  logToConsole("Indentation Error on line " + (line+1) + ": Expected '" + '-+*'.charAt((numSpaces-1)%3) + "', recieved '" + commands[line2].charAt(numSpaces + 1));
+                  threwError = true;
+                  break
+                }
+                indentLevel2 = numSpaces;
+              }
+              if (line2 < commands.length && !threwError) {
+                line = line2 - 1;
+              }
             }
           }
         }
@@ -286,13 +409,5 @@ function parse() {
       }
     }
     line++;
-  }
-}
-
-if (!DEBUG) {
-  logToConsole = x => {}
-} else {
-  logToConsole = function (text) {
-    document.getElementById("console").value += '> ' + text + "\n";
   }
 }
